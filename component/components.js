71b8a5ba83de0591bd3df6c82c1bb4368a094af6
Tim195 <readera193@gmail.com>
2022-08-15 10:28:57 +0800

    feat: rule update

--- component/components.js
+++ component/components.js
@@ -55,21 +55,33 @@ const Rule = () => {
             <Modal isVisible={isModalVisible} onBackdropPress={closeModal}>
                 <View style={styles.ruleContainer}>
                     <Text>
-                        遊戲初始設置：每位玩家一人一張角色卡{"\n\n"}
+                        遊戲初始設置：{"\n"}
+                        16 張角色牌洗牌後，2 人玩家需隨機移除 3 張角色牌，3~4 人玩家則隨機移除 1 張角色牌。{"\n"}
+                        每人抽一張角色卡做為起始手牌。{"\n\n"}
                         回合流程：{"\n"}
                         {"\t\t"}一、從牌庫中抽一張牌。
                     </Text>
                     <View style={{ flexDirection: "row" }}>
                         <Text>{"\t\t"}二、</Text>
-                        <Text numberOfLines={2}>從手中的兩張牌中打出一張，並執行該張牌的效果。</Text>
+                        <Text style={{ flex: 1 }}>從手中的兩張牌中打出一張，並執行該張牌的效果。</Text>
                     </View>
                     <View style={{ flexDirection: "row" }}>
                         <Text>{"\t\t"}三、</Text>
-                        <Text numberOfLines={2}>若抽完牌庫，且剩餘兩位以上玩家，則手牌最大者獲勝。</Text>
+                        <Text style={{ flex: 1 }}>若抽完牌庫，且剩餘兩位以上玩家，則手牌最大者獲勝。{"\n"}</Text>
+                    </View>
+                    <Text>補充：</Text>
+                    <View style={{ flexDirection: "row" }}>
+                        <Text>{"\t\t"}一、</Text>
+                        <Text style={{ flex: 1 }}>1/2/3/6 角色牌打出時，若其餘所有玩家皆被侍女保護，則直接打出沒有效果。</Text>
                     </View>
                     <View style={{ flexDirection: "row" }}>
-                        <Text style={{ textDecorationLine: "line-through" }}>{"\t\t"}四、</Text>
-                        <Text style={{ textDecorationLine: "line-through" }} numberOfLines={2}>伯爵夫人改成皇后，卡牌剩餘數量那邊比較方便排版......</Text>
+                        <Text>{"\t\t"}二、</Text>
+                        <Text style={{ flex: 1 }}>5 打出時，若其餘所有玩家皆被侍女保護，則捨棄自己手牌再抽一張。效果發動時，如果牌庫已抽完，改為抽遊戲開始前被移出的牌。</Text>
+                    </View>
+
+                    <View style={{ flexDirection: "row" }}>
+                        <Text style={{ textDecorationLine: "line-through" }}>{"\t\t"}三、</Text>
+                        <Text style={{ flex: 1, textDecorationLine: "line-through" }}>伯爵夫人改成皇后，卡牌剩餘數量那邊比較方便排版......</Text>
                     </View>
                 </View>
                 <Text style={styles.closeHint} onPress={closeModal}>
